d5323f81def5807e0791b914b773f03aa73f642e
Ben Gourley <bn@grly.me>
2017-11-24 14:17:35 +0000

    Port component name formatter over from Vue's source

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 const createPlugin = module.exports.createPlugin = (Vue) => ({
   init: (client, BugsnagReport) => {
-    Vue.config.errorHandler = (err, vm, info) => {
+    Vue = Vue || window.Vue
+    if (!Vue) return client._logger.warn('vue plugin did not receive a reference to Vue')
+
+    const prev = Vue.config.errorHandler
+
+    const handler = (err, vm, info) => {
       const handledState = { severity: 'error', unhandled: true, handledState: { type: 'unhandledException' } }
       const report = new BugsnagReport(err.name, err.message, BugsnagReport.getStacktrace(err), handledState)
+
       report.updateMetaData('vue', {
         errorInfo: info,
-        component: vm ? vm.$options.name || vm.$options._componentTag || 'app root' : undefined,
+        component: vm ? formatComponentName(vm) : undefined,
         props: vm ? vm.$options.propsData : undefined
       })
+
       client.notify(report)
       console.error(err)
+
+      if (typeof prev === 'function') prev.call(this, err, vm, info)
     }
+
+    Vue.config.errorHandler = handler
   }
 })
+
+// taken and reworked from Vue.js source
+const formatComponentName = (vm, includeFile) => {
+  if (vm.$root === vm) return '<Root>'
+  const options = typeof vm === 'function' && vm.cid != null
+    ? vm.options
+    : vm._isVue
+      ? vm.$options || vm.constructor.options
+      : vm || {}
+  let name = options.name || options._componentTag
+  const file = options.__file
+  if (!name && file) {
+    const match = file.match(/([^/\\]+)\.vue$/)
+    name = match && match[1]
+  }
+
+  return (
+    (name ? ('<' + (classify(name)) + '>') : '<Anonymous>') +
+    (file && includeFile !== false ? (' at ' + file) : '')
+  )
+}
+
+// taken and reworked from Vue.js source
+const classify = str => str.replace(/(?:^|[-_])(\w)/g, c => c.toUpperCase()).replace(/[-_]/g, '')
 
 module.exports = createPlugin(window.Vue)
